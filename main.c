fd020c10711a3cbdd84aa6284c963c1fd70977b4
wilson030402 <wilson.ranganathan@gmail.com>
2023-01-04 20:03:11 +0100

    affichage reussi

--- main.c
+++ main.c
@@ -4,22 +4,35 @@
 #include "clientAPI.h"
 
 
+
+//void PlayMove ( int* lab, int* tileN, int* tileE, int* tileS, int* tileW, int* tileItem){
+
+
 int main (void) {
 
 	
 	int TailleX;
 	int TailleY;
-	char* labyrinthNom;
-	connectToServer("172.105.76.204",1234,"Wilson");
-	
+	char labyrinthNom[50];
+	connectToServer("172.105.76.204",1234,"Yeogeuch");
 	
 	waitForLabyrinth("TRAINING DONTMOVE",labyrinthNom,&TailleX,&TailleY);
 	
-	//getLabyrinth(int* lab, int* tileN, int* tileE, int* tileS, int* tileW, int* tileItem);
-
+	int tuileN;
+	int tuileE;
+	int tuileS;
+	int tuileW;
+	int tuileItem;
+	
+	int* laby;
+	laby=malloc( (TailleX*TailleY*5)*sizeof(int) );
+		
+	getLabyrinth(laby, &tuileN, &tuileE, &tuileS, &tuileW, &tuileItem);
+	printLabyrinth();
+	printf ("%d %d %d %d %d\n", tuileN, tuileE,tuileS,tuileW,tuileItem);
 
 
 	closeConnection();
-	printf("Arrive ici\n");
+
 	return 0;
 }
